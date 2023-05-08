144504d19e8451060382401201eb0852555436f0
yundong.zhang <yundong.zhang@klarna.com>
2023-05-08 13:42:03 +0800

    111

---
+++ src/main/java/com/isyundong/Jenkins001Application.java
@@ -0,0 +1,13 @@
+package com.isyundong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/isyundong/Jenkins001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isyundong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
